f9175b2eb361f531dbfce2d0141abd815ae31116
Yaroslav Nikitenko <metst13@gmail.com>
2018-07-16 17:59:52 +0300

    Add spectra division to utils.h
    Check is done for spectra to have same vector size and energy scale.

--- utils.h
+++ utils.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <stdexcept>
 using namespace std;
 
 typedef std::vector<std::pair<double, double> > spectrum_vector; // spectrum<Energy, NEvents>, can be used for other functions.
@@ -13,6 +14,37 @@ void print_spectrum(const char *foutname, const char *header, spectrum_vector *s
     for(int i = 0; i < spectr->size(); ++i) {
         fout << (*spectr)[i].first << sep << (*spectr)[i].second << endl;
     }
+}
+
+spectrum_vector *spectrum_div(const spectrum_vector *num_sp, const spectrum_vector *denom_sp) {
+    // check that two spectra scale and size correspond
+    int nbins = num_sp->size();
+    if (denom_sp->size() != nbins) 
+        throw std::length_error("spectrum vectors should have same size.");
+    if (nbins)
+        if ((*num_sp)[0].first != (*denom_sp)[0].first
+            || (*num_sp)[nbins - 1].first != (*denom_sp)[nbins - 1].first)
+                throw std::logic_error("spectrum vectors should have same scale.");
+
+    // skip interval where denominator spectrum is zero
+    int nzeroes = 0;
+    while ((*denom_sp)[nzeroes].second == 0) 
+        ++nzeroes;
+    nbins -= nzeroes;
+
+    spectrum_vector *sp = new spectrum_vector(nbins);
+    int nnonzeroes = 0;
+    // divide spectra while denominator spectrum is nonzero
+    for (;nnonzeroes < nbins; ++nnonzeroes) {
+        int i = nnonzeroes + nzeroes;
+        double denom_sp_val = (*denom_sp)[i].second;
+        if (denom_sp_val == 0)
+            break;
+        (*sp)[nnonzeroes].first = (*num_sp)[i].first;
+        (*sp)[nnonzeroes].second =(*num_sp)[i].second / denom_sp_val;
+    }
+    sp->resize(nnonzeroes);
+    return sp;
 }
 
 #endif
